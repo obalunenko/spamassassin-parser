0e64aadcc25e645753e4f113708427fbe9526d44
Oleg Balunenko <oleg.balunenko@gmail.com>
2020-03-24 22:05:49 +0300

    fix(): Add docker login to release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,8 @@ git fetch --tags
 
 # Get the latest tag name
 latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
-echo ${latestTag}
+echo "${latestTag}"
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 
 curl -sL https://git.io/goreleaser | bash
